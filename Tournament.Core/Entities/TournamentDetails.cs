5d614fd5870f9cafb2d31f1d7314f26d17fbb094
Maureen <maureendelafuente.b@gmail.com>
2024-12-02 17:14:59 +0100

    Add project files.

---
+++ Tournament.Core/Entities/TournamentDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace Tournament.Core.Entities;
+
+public class TournamentDetails
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+
+    //navigation properties: because its a collection
+    public ICollection<Game> Games { get; set; }               
+    
+    
+}
