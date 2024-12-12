54e3c2a91a414a44b6ca8b89fac21bc286c98ab9
Maureen <maureendelafuente.b@gmail.com>
2024-12-12 19:42:12 +0100

    Moved controllers to Tournament.Presentation

--- Tournament.Presentation/Controllers/GamesController.cs
+++ Tournament.Presentation/Controllers/GamesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Service.Contracts;
 using Tournament.Core.Dto;
